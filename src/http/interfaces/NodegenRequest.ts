5381433935ecf1c2cbcac83ff9d077660c5b5415
John Carmichael <johncarmichael1984@gmail.com>
2025-11-10 15:55:14 +0000

    chore: remove non-required flex in the config

--- src/http/interfaces/NodegenRequest.ts
+++ src/http/interfaces/NodegenRequest.ts
@@ -1,22 +1,25 @@
 import express from 'express';
 import { IAPUserData } from '@/http/nodegen/middleware/iapAuthMiddleware';
-{% if nodegenRc.helpers.stub.jwtType %}import { {{ nodegenRc.helpers.stub.jwtType }} } from '@/http/nodegen/interfaces';{% endif %}
-{% if nodegenRc.helpers.stub.requestTypeExtensionPath %}import Ext from '{{ nodegenRc.helpers.stub.requestTypeExtensionPath }}';
 
-{% endif %}
 declare global {
   namespace Express {
     export interface Request {
-      jwtData: {{ nodegenRc.helpers.stub.jwtType if nodegenRc.helpers.stub.jwtType else 'any' }};
+      jwtData: any;
       originalToken: string;
       clientIp?: string;
       iapUser?: IAPUserData;
 
       /** If content-negotiation fails, default to this Content-Type instead of throwing. Can be set in the domain. */
       defaultContentType?: string;
+
+      // From RequestExtension
+      sessionData: {
+        sessionId: string;
+        userId: string;
+      };
     }
   }
 }
 
-type NodegenRequest = express.Request{% if nodegenRc.helpers.stub.requestTypeExtensionPath %} & Ext{% endif %};
+type NodegenRequest = express.Request;
 export default NodegenRequest;
